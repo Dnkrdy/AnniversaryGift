2ede46a353e25ede5ee21fa26b50bc8fe84667b0
Dnkrdy <dnkrdy@gmail.com>
2025-02-24 15:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let countdown = 10;
     const timerElement = document.getElementById("timer");
     let countdownStarted = false;
-    const musicDelay = 21000;
+    const musicDelay = 22000;
     const countdownSpeed = 2100;
     function updateCountdown() {
         if (countdown > 1) {
